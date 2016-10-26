374553f291f3eac446d44fcc57d010014e7cc4a8
Xavi Giménez <javiergimenezsanchez@gmail.com>
2016-10-26 22:51:12 +0200

    End of the day

--- Chapter02/Ch02_Basics/Program.cs
+++ Chapter02/Ch02_Basics/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Reflection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,31 @@ namespace Ch02_Basics
     {
         static void Main(string[] args)
         {
+            System.Data.SqlClient.SqlConnection connection;
+            System.Xml.XmlReader reader;
+            System.Xml.Linq.XElement element;
+            System.Net.Http.HttpClient client;
 
-        }
+            // Loop through the assemblies that this application references
+            foreach (var r in Assembly.GetExecutingAssembly().GetReferencedAssemblies())
+            {
+                // load the assembly wo we can read its details
+                var a = Assembly.Load(r.FullName);
+                // declare and  set a variable to count the total number of methods
+                int methodCount = 0;
+                // loop throuh all the types in the assembly
+                foreach( var t in a.DefinedTypes)
+                {
+                    // add up the counts of methods
+                    methodCount += t.GetMethods().Count();
+                }
+                // output the count of types and their methods
+                Console.WriteLine($"{a.DefinedTypes.Count():N0} types with {methodCount:N0} methods in {r.Name} assembly.");
+            }
+
+            // Declaring variables
+            double heightInMeters = 1.88;
+            Console.WriteLine($"The variable {nameof(heightInMeters)} has the value {heightInMeters}.");
+        } 
     }
 }
