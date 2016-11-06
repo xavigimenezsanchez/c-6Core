c87674cad9b3ca6fbea6b8baaf88d9ee40e43302
Xavi Giménez <javiergimenezsanchez@gmail.com>
2016-11-06 08:35:04 +0100

    End chapter

---
+++ Chapter04/Ch04_Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+
+namespace Ch04_Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\d+$");
+            do
+            {
+                WriteLine("The default regualar expression checks for at least one digit.");
+                Write("Enter a regular expression (or press ENTER to user the default):");
+                string regexStrin = ReadLine();
+                try
+                {
+                    regex = new Regex(regexStrin);
+                } catch (ArgumentException)
+                {
+                    regex = new Regex(@"\d+$");
+                }
+                Write("Enter Some input: ");
+                string input = ReadLine();
+                WriteLine($"{input} maches {regex}? {regex.IsMatch(input)}");
+                Write("Enter ESC to end or any key to try again");
+
+            } while (ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Chapter04/Ch04_Lists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new List<string>();
+            cities.Add("London");
+            cities.Add("Paris");
+            cities.Add("Milan");
+            WriteLine("Initial list");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+            WriteLine($"The first city is {cities[0]}.");
+            WriteLine($"The las city is {cities[cities.Count - 1]}.");
+            cities.Insert(0, "Sydney");
+            WriteLine("After inserting sydney at index 0");
+            foreach (string city in cities)
+            {
+                WriteLine($"   {city}");
+            }
+            cities.RemoveAt(1);
+            cities.Remove("Milan");
+            WriteLine("After removing two cities");
+            foreach (string city in cities)
+            {
+                WriteLine($"   {city}");
+            }
+        }
+    }
+}
